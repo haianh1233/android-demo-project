b44ddbd55e5d91bb642fb2e2598b105a2e3d44dc
haianh1233 <anhndhse151389@fpt.edu.vn>
2022-06-05 02:21:22 +0700

    add menu

--- app/src/main/java/com/haianh/demoproject/MainActivity.java
+++ app/src/main/java/com/haianh/demoproject/MainActivity.java
@@ -1,11 +1,15 @@
 package com.haianh.demoproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,11 +38,11 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, welcomeStatement, Toast.LENGTH_LONG).show();
         }
 
-        exitButton = findViewById(R.id.exit_button);
-        exitButton.setOnClickListener(v -> {
-            setResult(LoginActivity.EXIT_CODE, intent);
-            finish();
-        });
+//        exitButton = findViewById(R.id.exit_button);
+//        exitButton.setOnClickListener(v -> {
+//            setResult(LoginActivity.EXIT_CODE, intent);
+//            finish();
+//        });
 
         RecyclerView recyclerView = findViewById(R.id.main_view);
         recyclerView.setHasFixedSize(true);
@@ -55,5 +59,28 @@ public class MainActivity extends AppCompatActivity {
         Adapter adapter = new Adapter(list, getApplicationContext());
 
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_scrolling, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout: {
+                Intent intent = getIntent();
+                setResult(LoginActivity.EXIT_CODE, intent);
+                finish();
+                return true;
+            }
+            default:
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
